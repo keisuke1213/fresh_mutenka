022d4f4f4459207952a4f51384543c8862c86e9b
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-03-08 11:35:19 +0900

    必要なフォルダ及びファイル作成

---
+++ fresh_mutenka/src/app/list/page.tsx
@@ -0,0 +1,5 @@
+import List from "@/components/list/list";
+
+export default function Page() {
+    return <List />
+}
---
+++ fresh_mutenka/src/app/share/page.tsx
@@ -0,0 +1,5 @@
+import Share from "@/components/share/share";
+
+export default function Page() {
+    return <Share />
+}
