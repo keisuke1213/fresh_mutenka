ec26c29a642e1329342c855c831cb4bac3af7e0c
“ojiro-doi” <sakura.0046@icoud.com>
2024-03-09 13:54:08 +0900

    apiの取得から表示を行いました

--- fresh_mutenka/src/components/list/list.tsx
+++ fresh_mutenka/src/components/list/list.tsx
@@ -1,92 +1,27 @@
 'use client'
 
 import styles from "./list.module.css"
-import useSWR from 'swr';
 
-const lists = [
-    {
-        number: 100,
-        text: "a"
-    }, {
-        number: 90,
-        text: "b"
-    }, {
-        number: 80,
-        text: "a"
-    }, {
-        number: 70,
-        text: "b"
-    }, {
-        number: 60,
-        text: "a"
-    }, {
-        number: 50,
-        text: "b"
-    }, {
-        number: 40,
-        text: "b"
-    }, {
-        number: 30,
-        text: "a"
-    }, {
-        number: 20,
-        text: "b"
-    }, {
-        number: 10,
-        text: "",
-    },
-]
-
-export default function List() {
-    const fetcher = (url: string | URL | Request) => fetch(url).then(r => r.json())
-    const useUser = () => {
-        const { data, isLoading, error } = useSWR(`/api/user`, fetcher)
-
-        return {
-            user: data,
-            isLoading,
-            isError: error
-        }
-    }
-
-    const MessageData = () => {
-        const { user, isLoading, isError } = useUser()
-
-        if (isLoading) return <div>error</div>
-        if (isError) return <div>loading...</div>
-        return (
-            <div>a{user.message}</div>
-        )
-    }
-
-    let messageData=MessageData()
-
-    const NumberData =()=>{
-        const { user, isLoading, isError } = useUser()
-
-        if (isLoading) return <div>error</div>
-        if (isError) return <div>loading...</div>
-        return (
-            user.number
-        )
-    }
-
-    let numberData=NumberData()
-    console.log(numberData)
+type ListProps = {
+    goals:any
+    numberData:number
+}
+export default function List(props:ListProps) {
+    const {goals, numberData} = props
 
     return (
         <div>
             <div>
-                <MessageData />
+                {/* {user.number} */}
             </div>
             <div className="component-list">
                 <table className={styles.list}>
                     <tbody>
-                        {lists.map((list) => {
+                        {goals.map((goal:any) => {
                             return (
-                                <tr className={list.number === numberData ? styles.nowlist : ""} key={list.number}>
-                                    <td>{list.number}</td>
-                                    <td >{list.text}</td>
+                                <tr className={goal.number === numberData ? styles.nowlist : ""} key={goal.number}>
+                                    <td>{goal.number}</td>
+                                    <td >{goal.text}</td>
                                 </tr>
                             )
                         })}
--- fresh_mutenka/src/pages/api/user/index.ts
+++
@@ -1,3 +0,0 @@
-export default function hundler(req, res) {
-    res.status(200).json({number: 20,message: "hello"})
-}
