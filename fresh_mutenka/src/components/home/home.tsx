4a396ff12cbff30416374dc24f41700c591db4bd
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-03-10 18:27:33 +0900

    homeの修正

--- fresh_mutenka/src/components/home/home.tsx
+++ fresh_mutenka/src/components/home/home.tsx
@@ -2,24 +2,15 @@ import Header from "../common/Header/header";
 import FetchUserId from "../common/sidenav/fetchUserId";
 import List from "../list/list";
 import Chat from "./chat/chat";
-import style from './home.module.css'
 
 export default function Home() {
   
     return(
-        <div className={style.container} >
-            <div className={style.header} >
-                <Header />
-            </div>
-            <div className={style.main}>
-                <div className={style.fetchUserId}>
-                    <FetchUserId />
-                </div>
-                {/* <List />  */}
-                <div className={style.chat}>
-                    <Chat />
-                </div>
-            </div>
+        <div>
+            <Header />
+            <FetchUserId />
+            {/* <List />  */}
+            <Chat />  
         </div>
     ) 
 }
