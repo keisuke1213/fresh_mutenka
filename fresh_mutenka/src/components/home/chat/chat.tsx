4a0db2a549133db19d4193f6df43befac5a49453
Keisuke1586 <fushanguifu@gmail.com>
2024-03-10 23:25:58 +0900

    ✨ プロンプトの作成

--- fresh_mutenka/src/components/home/chat/chat.tsx
+++ fresh_mutenka/src/components/home/chat/chat.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import useSWR from "swr";
 import  { useState } from "react";
 import {getResponse} from "../../../pages/api/chat"
-import { testMethod } from "../../../pages/api/chat";
+
 
 const fetcher = (url:string) => fetch(url).then(r => r.json())
 const user_id = "7677e7cd-f1bc-4124-a42d-e80f5da3fedd"
@@ -14,12 +14,13 @@ type ConversationType = {
 }
 
 export default function Chat() {
+   
    const goalCreateApi = ()=> {
         const { data, error } = useSWR(`/api/goal/create/${user_id}`, fetcher)
         console.log(data);
       return data
    }
-   goalCreateApi();
+  //  goalCreateApi();
 
   const [userText, setUserText]   = useState("");
   const [messages, setMessages] = useState<ConversationType[]>([]);
@@ -30,6 +31,8 @@ export default function Chat() {
     console.log("サブゴール", sub_goal_id);
     return sub_goal_id
   }
+
+
   const onClickAddText = async () => {
     const res = await getResponse(userText);
     console.log("くりっく");
