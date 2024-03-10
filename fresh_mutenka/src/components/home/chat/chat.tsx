44873482c4342e6858d9d2a70a44ad59789d619a
Keisuke1586 <fushanguifu@gmail.com>
2024-03-10 12:06:11 +0900

    ✨ 空のgoalCreateApiを叩くことができた

--- fresh_mutenka/src/components/home/chat/chat.tsx
+++ fresh_mutenka/src/components/home/chat/chat.tsx
@@ -1,9 +1,12 @@
 "use client"
 import { useEffect } from "react";
+import useSWR from "swr";
 import  { useState } from "react";
 import {getResponse} from "../../../pages/api/chat"
 import { testMethod } from "../../../pages/api/chat";
 
+const fetcher = (url:string) => fetch(url).then(r => r.json())
+const user_id = "7677e7cd-f1bc-4124-a42d-e80f5da3fedd"
 
 type ConversationType = {
   user:string;
@@ -11,6 +14,13 @@ type ConversationType = {
 }
 
 export default function Chat() {
+   const goalCreateApi = ()=> {
+        const { data, error } = useSWR(`/api/goal/create/${user_id}`, fetcher)
+        console.log(data);
+      return data
+   }
+   goalCreateApi();
+
   const [userText, setUserText]   = useState("");
   const [messages, setMessages] = useState<ConversationType[]>([]);
   
