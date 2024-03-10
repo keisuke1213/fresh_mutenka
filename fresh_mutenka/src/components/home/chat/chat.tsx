85bc0f575b78028a71b7500280647f701c8f08ff
“ojiro-doi” <sakura.0046@icoud.com>
2024-03-10 21:02:45 +0900

    chatのUI設計しました

--- fresh_mutenka/src/components/home/chat/chat.tsx
+++ fresh_mutenka/src/components/home/chat/chat.tsx
@@ -1,31 +1,32 @@
 "use client"
+import styles from './chat.module.css'
 import { useEffect } from "react";
 import useSWR from "swr";
-import  { useState } from "react";
-import {getResponse} from "../../../pages/api/chat"
+import { useState } from "react";
+import { getResponse } from "../../../pages/api/chat"
 import { testMethod } from "../../../pages/api/chat";
 
-const fetcher = (url:string) => fetch(url).then(r => r.json())
+const fetcher = (url: string) => fetch(url).then(r => r.json())
 const user_id = "7677e7cd-f1bc-4124-a42d-e80f5da3fedd"
 
 type ConversationType = {
-  user:string;
-  chatgpt:string;
+  user: string;
+  chatgpt: string;
 }
 
 export default function Chat() {
-   const goalCreateApi = ()=> {
-        const { data, error } = useSWR(`/api/goal/create/${user_id}`, fetcher)
-        console.log(data);
-      return data
-   }
+  const goalCreateApi = () => {
+    const { data, error } = useSWR(`/api/goal/create/${user_id}`, fetcher)
+    console.log(data);
+    return data
+  }
   //  goalCreateApi();
 
-  const [userText, setUserText]   = useState("");
+  const [userText, setUserText] = useState("");
   const [messages, setMessages] = useState<ConversationType[]>([]);
-  const { data, error,  mutate } = useSWR(`/api/goal/getCurrentGoal/${user_id}`, fetcher)
+  const { data, error, mutate } = useSWR(`/api/goal/getCurrentGoal/${user_id}`, fetcher)
 
-    function getGoalId(){
+  function getGoalId() {
     const sub_goal_id = data.goals[0].id
     console.log("サブゴール", sub_goal_id);
     return sub_goal_id
@@ -34,9 +35,9 @@ export default function Chat() {
     const res = await getResponse(userText);
     console.log("くりっく");
     mutate();
-    
-    if (res?.includes("おめでとう") ) {
-  
+
+    if (res?.includes("おめでとう")) {
+
       getGoalId();
       // const getSubGoalId = (sub_goal_id:string) =>{
       //   getGoalId();
@@ -45,36 +46,35 @@ export default function Chat() {
       // }
       // getSubGoalId(sub_goal_id);
     }
-   
 
-    setMessages([...messages, {user:userText, chatgpt:res !== null ? res : ""}]);
+
+    setMessages([...messages, { user: userText, chatgpt: res !== null ? res : "" }]);
     setUserText("")
   }
   return (
-    <div>
-        <div>
-            {messages.map((message,index)=>{
-              return (
-                <div>
-                  <div>
-                    <p key={index}>You</p>
-                    <p>{message.user}</p>
-                  </div>
+    <div className={styles.chatContainer}>
+      <div className={styles.messageWrapper}>
+        {messages.map((message, index) => {
+          return (
+            <div>
+              <div className={styles.userWrapper}>
+                <p className={styles.nameYou} key={index}>You</p>
+                <p className={styles.messageYou}>{message.user}</p>
+              </div>
 
-                  <div>
-                    <p>Chat</p>
-                    <p>{message.chatgpt}</p>
-                  </div>
-                </div>
+              <div className={styles.chatGPTWrapper}>
+                <p className={styles.nameChatGPT}>Chat</p>
+                <p className={styles.messageChatGPT}>{message.chatgpt}</p>
+              </div>
+            </div>
 
-              );
-            })}
-        </div>
-        <div>
-
-        </div>
-        <input value={userText} type="text" onChange={(event)=>setUserText(event.target.value)}></input>
-        <button onClick={onClickAddText}></button>
+          );
+        })}
+      </div>
+      <div className={styles.textWrapper}>
+        <input value={userText} type="text" onChange={(event) => setUserText(event.target.value)}></input>
+        <button className={styles.submitBtn} onClick={onClickAddText}><img src="/button.png" alt="#" /></button>
+      </div>
     </div>
   );
 };
